8f9790a07ceece3184fc35487a70584804e63dbf
Mohamed Hammad <midoeljoker30@gmail.com>
2025-06-12 19:50:11 +0300

    asdfafd

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const express = require("express");
-const adminRouter = require("./routers/admin.route");
-const contactRouter = require("./routers/contact.route");
-const projectRouter = require("./routers/project.route");
-const app = express();
+const express = require('express')
 
-app.route("api/admin", adminRouter);
-app.route("api/contact", contactRouter);
-app.route("api/projects", projectRouter);
+const app = express()
 
-app.listen(3001, () => {
-  console.log(`server running on http://localhost:3001/`);
-});
+
+
+
+app.listen(3001,()=>{
+    console.log(`server running on http://localhost:3001/`);
+})
