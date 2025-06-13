25482bf0879a31cfaf9c1a0d19d6cad6df026692
braa-emad <112783591+braa-emad@users.noreply.github.com>
2025-06-13 17:32:33 +0300

    remove .env

--- controller/project.controller.js
+++ controller/project.controller.js
@@ -1,14 +1,26 @@
 const Project = require("../models/project.schema");
-const getProjects = async (req, res) => { //swilam
+const getProjects = async (req, res) => {
   res.json({ data: "test" });
 };
 
-const createProject = async (req, res) => { //swilam
+const createProject = async (req, res) => {
   res.json({ data: "test" });
 };
 
 const updateProject = async (req, res) => {
-  
+  try{
+      const UpdatedProject = await Project.findOne({projectid:projectid});
+      const title=req.body.title | UpdatedProject.title;
+      const description = req.body.description | updateProject.description;
+      const pic = req.body.pic |  updateProject.pic;
+      const time_to_finish = req.body.time_to_finish | updateProject.time_to_finish;
+      const sponser = req.body.sponser | updateProject.sponser;
+      const status = req.body.status | updateProject.status;
+      
+      res.json({staus:200,message:"updated successfully",data:{}});
+  }catch(err){
+    
+  }
 };
 
 const delProject = async (req, res) => {
@@ -21,7 +33,7 @@ const delProject = async (req, res) => {
         data: {},
       });
     }
-    const removeProject = await Project.findByIdAndRemove(projectId);
+    const removeProject = await Project.findOne({projectId:projectId});
 
     if (!removedProject) {
       return res.json({ status: 404, message: "Project not found", data: {} });
